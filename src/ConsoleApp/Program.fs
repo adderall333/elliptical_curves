8b8437f725e3678dc462db21eeb0760c185ba06a
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-05-24 15:23:29 +0300

    devide for different projects

---
+++ src/ConsoleApp/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open Library.EllipticalCurves
+
+[<EntryPoint>]
+let main argv =
+    printfn "Set p"
+    let p = Console.ReadLine() |> int
+    
+    printfn "Set a"
+    let a = Console.ReadLine() |> int
+    
+    printfn "Set b"
+    let b = Console.ReadLine() |> int
+    Console.WriteLine()
+    
+    printfn $"{getResult p a b}"
+    
+    printfn "Press any key to exit"
+    Console.ReadKey() |> ignore
+    0
---
+++ src/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Library;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Message { get; set; }
+        public string Result { get; set; }
+        
+        public void OnGet()
+        {
+        }
+        
+        public ActionResult OnPostResult(int p, int a, int b)
+        {
+            if (p < 2)
+            {
+                Message = "Argument p was too small";
+                return Page();
+            }
+
+            if (a == 0 || b == 0)
+            {
+                Message = "Arguments a and b dont't have to be 0";
+                return Page();
+            }
+
+            Result = EllipticalCurves.getResult(p, a, b);
+            return Page();
+        }
+    }
+}
